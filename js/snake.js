677355356dba1b85b90ed1a15c08cac11cab1f4f
Hunter2454254 <126Hhaynes@frhsd.com>
2025-03-03 14:03:59 -0500

    Day 4

--- js/snake.js
+++ js/snake.js
@@ -1,28 +1,33 @@
-export const SNAKE_SPEED=2;
+import { getInputDirection } from "./input.js";
+
+
+export const SNAKE_SPEED = 2;
+
+
 const snakeBody = [
-    { x: 10, y: 11},
-    { x: 11, y: 11},
-    { x: 12, y: 11},
-    { x: 13, y: 11},
-    { x: 14, y: 11},
-    { x: 15, y: 11}
-
+  { x: 11, y: 11 }
 ]
-export function update(){
- for (let i = snakeBody.length - 2; i >= 0; i--) {
+let newSegments = 0;
+
+
+export function update() {
+  const inputDirection = getInputDirection();
+
+
+  for (let i = snakeBody.length - 2; i >= 0; i--) {
     snakeBody[i + 1] = { ...snakeBody[i] };
- }
+  }
 
 
-    snakeBody[0].x += 1;
-    snakeBody[0].y += 0;
+  snakeBody[0].x += inputDirection.x;
+  snakeBody[0].y += inputDirection.y;
 
 
 
 }
 
-export function draw(gameBoard){
-  snakeBody.forEach(segment =>  {
+export function draw(gameBoard) {
+  snakeBody.forEach(segment => {
     const snakeElement = document.createElement('div');
     snakeElement.style.gridRowStart = segment.y;
     snakeElement.style.gridColumnStart = segment.x;
@@ -30,3 +35,14 @@ export function draw(gameBoard){
     gameBoard.appendChild(snakeElement);
   })
 }
+
+export function expandSnake(amount) {
+  newSegments += amount;
+}
+
+export function onSnake(position) {
+  return snakeBody.some(segment => {
+    return equalPositions(segment, position);
+  })
+}
+
